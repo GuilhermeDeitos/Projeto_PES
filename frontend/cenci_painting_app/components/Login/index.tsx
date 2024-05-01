57ba3bd63de91854be696fde3cdd9cae0c17e197
Guilherme Deitos <guilherme.cascavel@gmail.com>
2024-05-01 17:11:19 -0300

    Alterações login yudi

--- frontend/cenci_painting_app/components/Login/index.tsx
+++ frontend/cenci_painting_app/components/Login/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
-import { Button, Input, Text, Card} from '@rneui/themed';
+import { Input, Text, Card } from '@rneui/themed';
 import React, { useState } from 'react';
-import { View, StyleSheet, Image} from 'react-native';
+import { View, StyleSheet, Image, Button, Pressable } from 'react-native';
 
 
 export function Login() {
@@ -17,7 +17,7 @@ export function Login() {
     };
 
     const handleSubmit = () => {
-        if(username && password) {
+        if (username && password) {
             alert(`Username: ${username}, Password: ${password}`);
         } else {
             alert('Please fill in the form');
@@ -28,26 +28,37 @@ export function Login() {
     return (
         <View>
             <Image source="https://www.specialdog.com.br/assets/uploads/images/gateirosdeprimeiraviagem.png" />
-             <Card style={styles.container}>
-            <Text h2 style={styles.mainText} >Login</Text>
-            <Input
-                placeholder="Username"
-                value={username}
-                onChangeText={handleUsernameChange}
-                
-            />
-            <Input
-                placeholder="Password"
-                secureTextEntry
-                value={password}
-                onChangeText={handlePasswordChange}
-                
-            />
-            <Button title="Login" onPress={handleSubmit} type="outline" radius={18}/>
-            <Text style={styles.forgotText}>Forgot Password</Text>
-        </Card>
+            <View style={styles.container}>
+                <View style={styles.boxcontainer}>
+                    <Text h1 style={styles.mainText} >Login</Text>
+                    <Input
+                        
+                        style={styles.inputbox}
+                        placeholder="Username"
+                        value={username}
+                        onChangeText={handleUsernameChange}
+                        inputContainerStyle={{borderBottomWidth:0, width:"100%"}} 
+
+
+                    />
+                    <Input
+                        style={styles.inputbox}
+                        placeholder="Password"
+                        secureTextEntry
+                        value={password}
+                        onChangeText={handlePasswordChange}
+                        inputContainerStyle={{borderBottomWidth:0, width:"100%"}}
+
+                    />
+                    <Pressable style={styles.button} onPress={handleSubmit}>
+                        <Text style={styles.logintext}>Login</Text>
+                    </Pressable>
+                    <Text style={styles.forgotText}>Forgot Password</Text>
+                </View>
+            </View>
         </View>
-       
+
+
     );
 };
 
@@ -57,20 +68,58 @@ const styles = StyleSheet.create({
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        width: '100%',
+        backgroundColor: "white",
+        width: "100%",
+        height: "100%",
     },
 
-    button: {
-        width: '80%',
-        height: 40,
+    inputbox:{
+        borderWidth:0.5,
+        borderRadius:10,
+        padding:5,
+        marginRight:"10%",
+        marginLeft:"10%",
+                
     },
+    boxcontainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '70%',
+        height: "60%",
+        backgroundColor: "#EEEEEE",
+        borderRadius: 10,
+        elevation:15,
+        shadowColor:"black",  
+    },
+
+    
+
+    button: {
+        width: '60%',
+        height: 40,
+        backgroundColor: "#58CDFF",
+        borderRadius: 30,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    logintext: {
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+
     mainText: {
         textAlign: 'center',
-        marginBottom: 20,
+        marginBottom: 60,
+        
+
+
     },
     forgotText: {
         textAlign: 'center',
         marginTop: 20,
-        color: 'gray',
+        color: 'grey',
     },
 });
