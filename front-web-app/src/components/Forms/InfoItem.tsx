658b7729d9cf67e784f53170cfa588fd3811657c
Rafael Pascoali <rafaelpascz@gmail.com>
2024-05-31 20:58:58 -0300

    modificado

--- front-web-app/src/components/Forms/InfoItem.tsx
+++ front-web-app/src/components/Forms/InfoItem.tsx
@@ -1,13 +1,37 @@
+import React from "react";
 import { Item } from "../../pages/Estoque";
-import { Typography,Container } from "@mui/material";
+import { Typography,Container,Table } from "@mui/material";
+
 
 
 export function InfoItem(itemData:Item) {
 
 
         return(
-                <div>
-                        {itemData.name}
-                </div>
+                <Container>
+                        <Table>
+                                <tbody>
+                                <tr>
+                                {itemData.image}
+                                </tr>
+                                <Typography>
+                                <text>Name: </text>{itemData.name}
+                                </Typography>
+                                <Typography>
+                                <text>Price: </text>{itemData.price}
+                                </Typography>
+                                <Typography>
+                                <text>In stock: </text>{itemData.stock}
+                                </Typography>
+                                <Typography>
+                                <text>Description: </text>{itemData.description}
+                                </Typography>
+                                <Typography>
+                                <text>Status: </text>{itemData.status}
+                                </Typography>
+                                </tbody>
+                        </Table>
+                </Container>
+
         )
 }
